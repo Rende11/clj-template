2ddfa716f61bdc5449853fb645b383c8fc0d48a7
rende11 <artamonovn@gmail.com>
2019-08-04 20:36:56 +0400

    add  test

--- src/clj_template/routes.clj
+++ src/clj_template/routes.clj
@@ -1,7 +1,7 @@
 (ns clj-template.routes
-  (:require [clj-template.routes.index :refer [index]]
-            [clj-template.routes.features :refer [features]]
-            [clj-template.routes.layout :refer [application]]))
+  (:require [clj-template.pages.index :refer [index]]
+            [clj-template.pages.features :refer [features]]
+            [clj-template.pages.layout :refer [application]]))
 
 (def routes
   {:GET       #'index
---
+++ test/clj_template/core_test.clj
@@ -0,0 +1,18 @@
+(ns clj-template.test.core-test
+  (:require
+   [clojure.test :refer :all]
+   [matcho.core :as matcho]
+   [clj-template.core :as core]
+   [ring.mock.request :as mock]))
+
+(deftest routes
+
+  (matcho/assert
+   {:status 200
+    :body not-empty}
+   (core/app (mock/request :get "/")))
+
+  (matcho/assert
+   {:status 200
+    :body not-empty}
+   (core/app (mock/request :get "/features"))))
